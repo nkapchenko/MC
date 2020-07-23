5bc2cf10d5c9f6e752f58705f4a92d224c33e2ce
nkapchenko <n.kapchenko@gmail.com>
2020-07-23 10:11:09 +0200

    IntegratedOI

--- src/mc/mc.py
+++ src/mc/mc.py
@@ -7,7 +7,7 @@ from mc.stochasticprocess import StochasticProcess
 from collections import namedtuple
 
 from hw import hw_helper
-
+from copy import deepcopy
 
 
 class BlackScholesProcess(StochasticProcess): # should be abc class
@@ -34,38 +34,25 @@ class BlackScholesProcess(StochasticProcess): # should be abc class
         return pd.DataFrame(diffusion, index = np.linspace(0, until, timestep + 1 ) )
 
 
-class HullWhiteProcess(StochasticProcess):
+class OrnsteinUhlenbeck(StochasticProcess):
 
-    def __init__(self, model, x0, measure):
+    def __init__(self, mr, sigma, x0, measure):
         """
-        model = hw.HullWhite
+        mr - mean reversion
+        sigma - rates.Curve PieceWise volatility
         x0 = float or array
         measure - float, 0. for Risk neutral measure, float for T-forward measure
         """
-        self.mr = model.mr
-        self.sigma = model.sigma
+        self.mr = mr
+        self.sigma = sigma
         self.x0 = x0
         self.measure = measure
         
-#         self.diffusion = None
-
             
-    @property
-    def size(self):
-        return 1
-
                   
     @property
     def initial_value(self):
         return self.x0
-    
-    @property
-    def drift_coef(self, t, x):
-        pass
-    
-    @property
-    def diffusion_coef(self, t, x):
-        return self.sigma
     
     
     def expectation(self, s, x0, dt):
@@ -81,22 +68,13 @@ class HullWhiteProcess(StochasticProcess):
         else: # T-forward 
             return x0 * exp( - self.mr * dt) - hw_helper.get_drift_T(s, s+dt, U=self.measure, a=self.mr, sigma=self.sigma)
         
-        
-    def stdDeviation(self, s, dt):
-        return np.sqrt(self.variance(s, dt))
     
     def variance(self, s, dt):
+        "measure independent"
         return hw_helper.get_var_x(T=s+dt, a=self.mr, sigma=self.sigma, s=s)
     
-    def covariance(self, s, dt):
-        " V(X(s+dt) | Xs = x0) "
-        return self.covariance(s, dt)
     
-    def evolve(self, s, x0, dt, dw):
-        pass
-    
-    
-    def generate_paths(self, until, timestep, spotstep, curiosity = False):
+    def diffuse(self, until, timestep, spotstep, curiosity = False):
         """
         return dataframe with monte carlo simulation using exact solution for Ornstein Uhlenbeck process (aka Xt).
         
@@ -117,12 +95,10 @@ class HullWhiteProcess(StochasticProcess):
         for (t_idx, t), rnd in zip(enumerate(time_axis), noise):
                 exact_diffusion[t_idx+1] = ( self.expectation(s=t, x0=exact_diffusion[t_idx], dt=dt) + self.stdDeviation(s=t, dt=dt) * rnd) 
                 
-        self.diffusion = pd.DataFrame(exact_diffusion, index = time_axis)  
-        
-        if not curiosity:
-            return self.diffusion
-        else:       
-            return self.diffusion, mc_curiosity(self, until, timestep, spotstep, time_axis , dt, noise)
+        self.diffusion = pd.DataFrame(exact_diffusion, index = time_axis)
+
+        self.curiosity = mc_curiosity(self, until, timestep, spotstep, time_axis , dt, noise) if curiosity else None
+            
     
     # TODO: this function should be defined inside HullWhite class
     def fwd_bond(self, t, T, dsc_curve, x): 
@@ -133,6 +109,31 @@ class HullWhiteProcess(StochasticProcess):
     
         
 
+    
+    
+class HullWhiteProcess(OrnsteinUhlenbeck):
+    
+    def __init__(self, ou, dsc_curve):
+        super().__init__(ou.mr, ou.sigma, ou.x0, ou.measure)
+        self.xt_diffusion = ou.diffusion
+        
+        self.dsc_curve = dsc_curve
+        self.curiosity = None
+    
+    def expectation(self, s, x0, dt):
+        return super().expectation(s, x0, dt) + hw_helper.get_phi(s+dt, self.mr, self.sigma) + self.dsc_curve(s+dt)
+    
+    @property
+    def diffuse(self):
+        
+        tt = self.xt_diffusion.index.values
+        phi_t = [hw_helper.get_phi(t, a=self.mr, sigma=self.sigma) for t in tt]
+        
+        self.diffusion = self.xt_diffusion.add(pd.Series(phi_t + self.dsc_curve(tt), index=tt), axis=0)
+        
+
+    
+    
 
 
 
@@ -140,3 +141,35 @@ class HullWhiteProcess(StochasticProcess):
     
 if __name__ == '__main__':
     print('df')
+    
+    
+    
+    
+class IntegratedOrnsteinUhlenbeck(OrnsteinUhlenbeck):
+    
+    def __init__(self, ou):
+        super().__init__(ou.mr, ou.sigma, ou.x0, ou.measure)
+        try: self.xt_diffusion = ou.diffusion
+        except: self.xt_diffusion = None 
+        self.curiosity = None
+        
+    @property
+    def initial_value(self):
+        return self.x0
+    
+    def expectation(self, s, x0, dt):
+        " TODO enrich doc consulting OU father class"
+        
+        if self.measure ==0 : #RN
+            return x0 * hw_helper._B(t=s, T=s+dt, a=self.mr)
+        else: # T-forward
+            return x0 * hw_helper._B(t=s, T=s+dt, a=self.mr) - hw_helper.get_integrated_drift_T(s, s+dt, U=self.measure, a=self.mr, sigma=self.sigma)
+        
+    def variance(self, s, dt):
+        return hw_helper._V(s, s+dt, self.measure, a=self.mr, sigma=self.sigma)
+        
+    
+    @property
+    def diffuse(self):
+        dt = self.xt_diffusion.index[1]
+        self.diffusion = self.xt_diffusion.cumsum() * dt
